9466a7baaab3554aaca543ed23ae060155817416
unknown <5ecant>
2025-08-28 20:36:05 +0900

    v0.5 datatype & caseting

---
+++ week03/src/datatype.java
@@ -0,0 +1,10 @@
+public class datatype {
+    public static void main(String[] args) {
+        float gpa = 4.13F;
+        int score = (int) 9.99;
+        System.out.println(gpa);
+        score = (int) gpa;
+        gpa = score;
+        System.out.println(score);
+    }
+}
