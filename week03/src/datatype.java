287e487cfd9469481275468592fbee806189b15f
unknown <5ecant>
2025-08-28 21:32:07 +0900

    v0.5 DataType & Casting

--- week03/src/datatype.java
+++ week03/src/datatype.java
@@ -1,5 +1,6 @@
 public class datatype {
     public static void main(String[] args) {
+        // DataType & Casting
         float gpa = 4.13F;
         int score = (int) 9.99;
         System.out.println(gpa);
