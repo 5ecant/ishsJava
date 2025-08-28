2ddd520fffae483551dc28dfea0b6d2f96d17a1e
5ecant <lukelyh@naver.com>
2025-08-28 21:41:16 +0900

    v0.8 Data type and casting

--- week03/src/datatype.java
+++ week03/src/datatype.java
@@ -1,5 +1,6 @@
 public class datatype {
     public static void main(String[] args) {
+        // 충돌 해결 하는 법
         // DataType & Casting (자료형과 형변환)
         float gpa = 4.13F;
         int score = (int) 9.99;
