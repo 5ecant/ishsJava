1f87c853a229ee0ff36253ee9d9f44264555a804
unknown <5ecant>
2025-09-04 20:30:13 +0900

    v1.2 if Else

--- week04/src/PoketmonGame.java
+++ week04/src/PoketmonGame.java
@@ -1,13 +1,25 @@
-class Poketmon{
+import java.util.Scanner;
+
+class Poketmon {
     int hp;
 }
 
 public class PoketmonGame {
     public static void main(String[] args) {
         Poketmon pikacu = new Poketmon();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("체력을 입력: ");
+        pikacu.hp = scanner.nextInt();
+
         Poketmon ggo = new Poketmon();
-        pikacu.hp = 100
         ggo.hp = 100;
-        System.out.println(poketmon.hp);
+        System.out.println(pikacu.hp);
+
+        if (pikacu.hp > 100) {
+            System.out.println("조직력이 낮습니다!");
+        }else{
+            System.out.println("ㅁㄴㅇㄹ");
+        }
     }
 }
