e28966797f7314ca5016ffc1ab49924c5cd1fa51
unknown <5ecant>
2025-09-04 21:10:33 +0900

    v1.3 dudu

--- week04/src/rurudanGame.java
+++ week04/src/rurudanGame.java
@@ -6,8 +6,12 @@ public class rurudanGame {
         System.out.print("");
         int dan = scanner.nextInt();
 
-        for (int i=1; i<10; i++) {
-            System.out.println(dan + " x " + i + " = " + (dan*i));
+        if (dan < 2 || dan > 9) {
+            System.out.println("범위를 벗어났습니다.");
+        }else{
+            for (int i=1; i<10; i++) {
+                System.out.println(dan + " x " + i + " = " + (dan*i));
+            }
         }
     }
 }
