6652b2d81bb2059543249506a1e8993ffaceb727
5ecant <lukelyh@naver.com>
2025-10-16 19:47:48 +0900

    v0.1 Poketmon Test

--- src/GameDemo.java
+++ src/GameDemo.java
@@ -1,5 +1,8 @@
 public class GameDemo {
     public static void main(String[] args) {
-
+        Pikachu pikachu = new Pikachu(100);
+        Pikachu yourPikachu = new Pikachu(100);
+        System.out.println("Game Start");
+        yourPikachu.attack(pikachu);
     }
 }
