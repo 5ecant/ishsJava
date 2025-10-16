d1ebeed9f7de46d431b2393f6363d16b1279da8a
unknown <5ecant>
2025-10-16 19:33:22 +0900

    week06

--- src/Pikachu.java
+++ src/Pikachu.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+public class Pikachu extends Poketmon{
+    public Pikachu(int hp) {
+        super("피카츄", hp);
+        System.out.println("피카피카");
+    }
 
-public class Pikachu {
+    @Override
+    public void attack(Poketmon target) {
+        // System.out.println(getName() + " -> " + target.getName()); // name is private
+        System.out.println(name + " -> " + target.getName() + "에게 전기충격 사용"); // name is protected
+    }
 }
--- src/Poketmon.java
+++ src/Poketmon.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+public abstract class Poketmon {
+    protected String name;
+    private int hp;
 
-public class Poketmon {
+    public Poketmon(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHP() {
+        return hp;
+    }
+
+    public abstract void attack(Poketmon target);
 }
